4768af8a7e59b84587ca99fc9f8fc18e81641712
watersro <watersstewartr@gmail.com>
2021-07-15 05:27:42 +0100

    update

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,20 +1,20 @@
 def bubble_sort(input)
-    loop do
-        sort_iter = input.length - 1
-        sorted = input.clone
-        i = 0
-            while i < sort_iter
-            first = input[i]
-            second = input[i+1]
-            if first > second
-                input[i] = second
-                input[i+1] = first                        
-            end
-            i += 1
-        end
-    break if input === sorted
+  loop do
+    sort_iter = input.length - 1
+      sorted = input.clone
+      i = 0
+      while i < sort_iter
+      first = input[i]
+      second = input[i+1]
+      if first > second
+          input[i] = second
+          input[i+1] = first
+      end
+      i += 1
     end
-    puts input.to_a
+  break if input === sorted
+  end
+puts input.to_a
 end
 
 bubble_sort([4,3,78,2,0,2])
